fc1b4ce69280cf564b5860b9c1d78811d987f8d6
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 23:35:03 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- post_update.sh
+++ post_update.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# $1 Path to the new version.
-# $2 Path to the old version.
-
-set -x
-set -e
-
-cp -a -n -r $2/android $1/
-
-# upstream OWNERS files are not AOSP Gerrit OWNERS files.
-rm -f `find grpc -name OWNERS`
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a31bd945c8d9df86a6e5a9a1365f7c3d6894c0c53ef111b122d7f9f0eef115e
+size 206
